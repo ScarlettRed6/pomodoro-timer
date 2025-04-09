a685373766e6b130261a2848f65f2bae3aca238d
Carl!! <jcaamrelo@gmail.com>
2025-04-09 13:10:44 +0800

    Fragment class Setup

--- app/src/main/java/com/example/pomodoro_timer/ui/fragments/TimerFragment.java
+++ app/src/main/java/com/example/pomodoro_timer/ui/fragments/TimerFragment.java
@@ -1,4 +1,23 @@
 package com.example.pomodoro_timer.ui.fragments;
 
-public class TimerFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.pomodoro_timer.R;
+
+public class TimerFragment extends Fragment {
+
+    public TimerFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_timer, container, false);
+    }
+
 }
