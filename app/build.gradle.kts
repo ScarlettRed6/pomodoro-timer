2bf0bd413e2c2b79ae9d1b5e9207fcfecf4e4973
DiabeticSugarVamp <israel.rodriguez.dev@gmail.com>
2025-05-01 16:40:22 +0800

    Added Room Dependencies for Plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.room.common.jvm)
     //Added new dependencies for fragments and navigation component
     val navVersion = "2.8.9"
 
@@ -51,6 +52,24 @@ dependencies {
     // If this project only uses Java source, use the Java annotationProcessor
     // No additional plugins are necessary
     annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // optional - RxJava2 support for Room
+    implementation("androidx.room:room-rxjava2:$room_version")
+
+    // optional - RxJava3 support for Room
+    implementation("androidx.room:room-rxjava3:$room_version")
+
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation("androidx.room:room-guava:$room_version")
+
+    // optional - Test helpers
+    testImplementation("androidx.room:room-testing:$room_version")
+
+    // optional - Paging 3 Integration
+    implementation("androidx.room:room-paging:$room_version")
 
     // Views/Fragments integration
     implementation("androidx.navigation:navigation-fragment:$navVersion")
